f918dcb6ab70e5c48d3334d060a8a814f5d0349d
Yana <106078240+Strawberry-LuLu@users.noreply.github.com>
2026-07-29 16:56:09 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,9 @@
+export default {
+  async fetch(request, env) {
+    if (request.method === "GET") {
+      return new Response("Buyer questionnaire bot is running");
+    }
 
+    return new Response("ok");
+  }
+};
